176138a4912e71e64455385f8745f53fab0c3cc6
NachinBombin <solidnachox@gmail.com>
2026-04-03 22:50:32 -0300

    Remove orphaned combine_sentry entity

--- lua/entities/combine_sentry/init.lua
+++
@@ -1,7 +0,0 @@
--- combine_sentry/init.lua
--- Exact same structure as sentry_m2hmg/init.lua.
--- All AI logic lives in sentry_base/init.lua via inheritance.
-
-AddCSLuaFile("cl_init.lua")
-AddCSLuaFile("shared.lua")
-include("shared.lua")
